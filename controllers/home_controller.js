f681b3dab0a47fad666984b3db77db2b79bb1e9f
shruti <shrutisinghal959@gmail.com>
2023-10-27 20:31:52 +0530

    display posts and author name too

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,30 @@
-
+const Post=require('../models/post');
 
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+//     Post.find({})
+//   .then((posts) => {
+//     // console.log(posts);
+//     return res.render('home', {
+//       title: "Codial || Home",
+//       posts: posts
+//     });
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+
+//populate the user of each post
+  Post.find({}).populate('user').exec()
+  .then((posts) => {
+        // console.log(posts);
+        return res.render('home', {
+          title: "Codial || Home",
+          posts: posts
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    
 }
